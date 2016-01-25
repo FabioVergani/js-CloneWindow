d428eea39ac2c0d5bb84262491bf4a62fa9d69c0
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-01-25 17:28:17 +0100

    Create 002.js

---
+++ 002.js
@@ -0,0 +1,18 @@
+			(function(win){
+				var $w=win, $d=$w.document, f=function(){
+				 var e, h, d=$d, w=$w, o=w.open('data:text/html;charset=utf-8,');
+				 w.removeEventListener('load',f);
+				 if(o!==e){
+					e=d.doctype;
+					h=d.documentElement;
+					d=o.document;
+
+					d.write(e===null?'<!DOCTYPE html>':new XMLSerializer().serializeToString(e));
+					d.close();
+
+					(e=d.createDocumentFragment().appendChild(h.cloneNode(true))).normalize();
+					d.replaceChild(e,d.documentElement);//no text nodes in the sub-tree are empty and there are no adjacent text nodes
+				 };
+				};
+				if($d.readyState==='complete'){f();}else{$w.addEventListener('load',f);};
+			})(window);
