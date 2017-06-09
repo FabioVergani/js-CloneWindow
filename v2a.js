3ca99f8e626a8203cb9f42c78a3e110e6e291281
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-09 19:13:37 +0200

    Update v2a.js

--- v2a.js
+++ v2a.js
@@ -1,26 +1,20 @@
-(function($){
+(function($) {
+ 
+  const $d=$.document,
+	$dt=$d.doctype,
+	$dl=$d.location,
+	$frag=$d.createDocumentFragment(),
+	$dE=$frag.appendChild($d.documentElement.cloneNode(true)),
+	$head=$dE.querySelector('head'),
+	$a=$frag.appendChild($d.createElement('a'));
 
- function absolutize(t){
-		const a=$a,p=r,v=t,n=$frag.querySelectorAll('['+v+']');
-		for(const x of n){
-			const s=x.getAttribute(v);
-			if(p.test(s)!==true){x.setAttribute(v,(a.href=s,a.href))}
+	for(let i=0,m=['href','src'];i<2;++i){
+		for(let e,j=0,s=m[i],n=$frag.querySelectorAll('['+s+']'),l=n.length,a=$a,p=/^((https?|data):|www\.)/i;j<l;++j){
+			const e=n[j],x=e.getAttribute(s);
+			if(p.test(x)!==true){console.dir(e);a.href=x;e.setAttribute(s,a.href)}
 		}
- }
+	};
 
- const r=/^((https?|data):|www\.)/i;
- $d=$.document,
- $dt=$d.doctype,
- $dl=$d.location,
- $frag=$d.createDocumentFragment(),
- $a=$frag.appendChild($d.createElement('a')),
- $dE=$frag.appendChild($d.documentElement.cloneNode(true)),
- $head=$dE.querySelector('head');
- //
- absolutize('href');
- absolutize('src');
- //
- $head.insertBefore($d.createElement('base'),$head.firstChild).href=[$dl.protocol,$dl.host].join('//');
- //
- $.open(URL.createObjectURL(new Blob([(new TextEncoder()).encode(['<!DOCTYPE\u0020',$dt.name,(x=$dt.publicId)?'\u0020PUBLIC\u0020"'+x+'"':'',(x=$dt.systemId)?'\u0020"'+x+'"':'','>',new XMLSerializer().serializeToString($dE)].join(''))],{type:'text/html'})));
+  $head.insertBefore($d.createElement('base'),$head.firstChild).href=[$dl.protocol,$dl.host].join('//');
+	let opened=$.open(URL.createObjectURL(new Blob([(new TextEncoder).encode('<!DOCTYPE\u0020'+$dt.name+((x=$dt.publicId)?'\u0020PUBLIC\u0020"'+x+'"':"")+((x=$dt.systemId)?'\u0020"' +x+'"':"")+'>'+(new XMLSerializer).serializeToString($dE))],{type:'text/html'})));
 })(window);
