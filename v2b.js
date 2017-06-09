dada8723eb7b75ac1a08db8b0fb0955b91e51165
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-09 19:44:14 +0200

    Update v2b.js

--- v2b.js
+++ v2b.js
@@ -1,4 +1,4 @@
-(function($) {
+(function($){
   const $d=$.document,
 	$dt=$d.doctype,
 	$dl=$d.location,
@@ -6,14 +6,17 @@
 	$dE=$frag.appendChild($d.documentElement.cloneNode(true)),
 	$head=$dE.querySelector('head'),
 	$a=$frag.appendChild($d.createElement('a'));
+	
+	$head.appendChild($d.createElement('script')).textContent='window.addEventListener("load",function(e){window.scrollTo('+$.scrollX+','+$.scrollY+')});';
 
 	for(let i=0,m=['href','src'];i<2;++i){
 		for(let e,j=0,s=m[i],n=$frag.querySelectorAll('['+s+']'),l=n.length,a=$a,p=/^((https?|data):|www\.)/i;j<l;++j){
 			const e=n[j],x=e.getAttribute(s);
-			if(p.test(x)!==true){console.dir(e);a.href=x;e.setAttribute(s,a.href)}
+			if(p.test(x)!==true){a.href=x;e.setAttribute(s,a.href)}
 		}
 	};
 
   $head.insertBefore($d.createElement('base'),$head.firstChild).href=[$dl.protocol,$dl.host].join('//');
-	let opened=$.open($.URL.createObjectURL(new $.Blob([(new $.TextEncoder).encode('<!DOCTYPE\u0020'+$dt.name+((x=$dt.publicId)?'\u0020PUBLIC\u0020"'+x+'"':"")+((x=$dt.systemId)?'\u0020"' +x+'"':"")+'>'+(new $.XMLSerializer).serializeToString($dE))],{type:'text/html',encoding:$d.characterSet})));
+	const w=$.open($.URL.createObjectURL(new $.Blob([(new $.TextEncoder).encode('<!DOCTYPE\u0020'+$dt.name+((x=$dt.publicId)?'\u0020PUBLIC\u0020"'+x+'"':"")+((x=$dt.systemId)?'\u0020"' +x+'"':"")+'>'+(new $.XMLSerializer).serializeToString($dE))],{type:'text/html',encoding:$d.characterSet}))), d=w.document;
 })(window);
+
